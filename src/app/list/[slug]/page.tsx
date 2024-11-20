7b87371e3c28e4c0743f5ace17b2b1f0a4cc9b0d
yatiac <master.152@gmail.com>
2024-11-20 10:59:55 -0500

    fix build error

--- src/app/list/[slug]/page.tsx
+++ src/app/list/[slug]/page.tsx
@@ -10,8 +10,14 @@ export default async function Page({
   const list = await getListBySlug(slug);
   return (
     <div>
-      <h1 className="pt-4 text-4xl font-bold text-center mb-8 text-primary">{ list.name }</h1>
-      <ProductCardsWithModal list={list} />
+      { list ? (
+        <>
+          <h1 className="pt-4 text-4xl font-bold text-center mb-8 text-primary">{ list.name }</h1>
+          <ProductCardsWithModal list={list} />
+        </>
+      ) : (
+        <h1 className="pt-4 text-4xl font-bold text-center mb-8 text-primary">List not found</h1>
+      )}
     </div>
   )
 }
